58b3d0f81cb53c3a40afc07596ddccc32271565f
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2023-05-15 16:56:07 +0900

    polish

--- example/actuator-issue/build.gradle.kts
+++ example/actuator-issue/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 dependencies {
     implementation("com.linecorp.armeria:armeria-spring-boot3-starter")
     implementation("com.linecorp.armeria:armeria-spring-boot3-actuator-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
--- example/actuator-issue/src/main/java/example/actuator_issue/Application.java
+++ example/actuator-issue/src/main/java/example/actuator_issue/Application.java
@@ -3,6 +3,10 @@ package example.actuator_issue;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.expose.EndpointExposure;
+import org.springframework.boot.actuate.autoconfigure.endpoint.expose.IncludeExcludeEndpointFilter;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -21,4 +25,13 @@ public class Application {
             builder.service("/", (ctx, req) -> HttpResponse.of("Hello, world!"));
         };
     }
+
+    // Workaround: Set this bean to enable WebEndpoint filters
+    // ref: https://github.com/spring-projects/spring-boot/blob/main/spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/WebEndpointAutoConfiguration.java#L112-L116
+//    @Bean
+//    public IncludeExcludeEndpointFilter<ExposableWebEndpoint> webExposeExcludePropertyEndpointFilter(WebEndpointProperties properties) {
+//        WebEndpointProperties.Exposure exposure = properties.getExposure();
+//        return new IncludeExcludeEndpointFilter<>(ExposableWebEndpoint.class, exposure.getInclude(),
+//                exposure.getExclude(), EndpointExposure.WEB.getDefaultIncludes());
+//    }
 }
